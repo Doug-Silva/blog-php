fc742e077fca4e9f653da541b0cb479ff227edbc
Douglas Silva <silvatec.br@gmail.com>
2022-06-29 23:03:12 -0300

    exibindo os artigos 2.0

--- config.php
+++ config.php
@@ -2,3 +2,7 @@
 
 $mysql = new mysqli('localhost:3307', 'root', '', 'blog');
 $mysql->set_charset('utf8');
+
+if ($mysql == FALSE) {
+    echo "Erro de conexão";
+}
--- src/Artigo.php
+++ src/Artigo.php
@@ -17,5 +17,16 @@ class Artigo
 
         return $resultado->fetch_all(MYSQLI_ASSOC);
     }
+
+    public function encontrarPorId(string $id): array
+    {
+        $selecionaArtigo = $this->mysql->prepare("SELECT id, titulo, conteudo FROM artigos WHERE id = ?");
+
+        $selecionaArtigo->bind_param('s', $id);
+
+        $selecionaArtigo->execute();
+
+        return $selecionaArtigo->get_result()->fetch_assoc();
+    }
 }
 
