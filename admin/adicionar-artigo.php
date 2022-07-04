8a1753aef7c592088855972fa3f1f83fc48434cf
Douglas Silva <silvatec.br@gmail.com>
2022-07-03 21:45:33 -0300

    editando um artigo

--- admin/adicionar-artigo.php
+++ admin/adicionar-artigo.php
@@ -26,11 +26,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <h1>Adicionar Artigo</h1>
             <form action="adicionar-artigo.php" method="post">
                 <p>
-                    <label for="">Digite o título do artigo</label>
+                    <label for="titulo">Digite o título do artigo</label>
                     <input class="campo-form" type="text" name="titulo" id="titulo" />
                 </p>
                 <p>
-                    <label for="">Digite o conteúdo do artigo</label>
+                    <label for="conteudo">Digite o conteúdo do artigo</label>
                     <textarea class="campo-form" type="text" name="conteudo" id="conteudo"></textarea>
                 </p>
                 <p>
